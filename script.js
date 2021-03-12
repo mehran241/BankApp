f9f013f414d399b458d953a589d6065f286bc5b2
Mehran Rauf <mehranrauf19@gmail.com>
2021-03-12 11:47:54 +0500

    creating DOM elements

--- script.js
+++ script.js
@@ -61,6 +61,22 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}"> 
+          ${i + 1} ${type}</div>
+          
+          <div class="movements__value">${mov}</div>
+        </div>`;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -105,6 +121,8 @@ console.log([...arr, ...arr2]);
 console.log(letters.join(' - '));
 */
 
+/*
+
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 for (const movement of movements) {
@@ -134,3 +152,4 @@ const currencies = new Map([
 currencies.forEach(function (items, index, map) {
   console.log(`${index} : ${items}`);
 });
+*/
