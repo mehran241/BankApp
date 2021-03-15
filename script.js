e42667bec14d73c1c4eeac4ffed41225cd70e5d4
Mehran Rauf <mehranrauf19@gmail.com>
2021-03-15 17:15:27 +0500

    implementing login

--- script.js
+++ script.js
@@ -70,12 +70,82 @@ const displayMovements = function (movements) {
           <div class="movements__type movements__type--${type}"> 
           ${i + 1} ${type}</div>
           
-          <div class="movements__value">${mov}</div>
+          <div class="movements__value">${mov}€</div>
         </div>`;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
-displayMovements(account1.movements);
+// displayMovements(account1.movements);
+
+const currDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+// currDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+// calcDisplaySummary(account1.movements);
+
+const createusername = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(function (word) {
+        return word[0];
+      })
+      .join('');
+  });
+};
+
+createusername(accounts);
+console.log(accounts);
+
+//  Event Handler
+
+let currentAccount;
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+
+  console.log(currentAccount);
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    console.log(`Login`);
+
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+    // Display movements
+    displayMovements(currentAccount.movements);
+    // Display balance
+    currDisplayBalance(currentAccount.movements);
+    // Display summary
+    calcDisplaySummary(currentAccount);
+  }
+});
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -84,8 +154,8 @@ displayMovements(account1.movements);
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-
-/* const arr = ['a', 'b', 'c', 'd', 'e'];
+/*
+const arr = ['a', 'b', 'c', 'd', 'e'];
 
 // slice method
 console.log(arr.slice(2));
@@ -104,7 +174,6 @@ console.log(arr.splice(-1));
 console.log(arr.splice(1, 2));
 console.log(arr);
 */
-
 // Reverse method
 /*
 const arr = ['a', 'b', 'c', 'd', 'e'];
@@ -152,4 +221,58 @@ const currencies = new Map([
 currencies.forEach(function (items, index, map) {
   console.log(`${index} : ${items}`);
 });
+
+const juliarr = [8, 5, 6, 12, 7];
+const checkDogs = function (dogsjulia, dogskate) {
+  const correctdogs = juliarr.slice(1, 3);
+  const dogs = correctdogs.concat(dogskate);
+  console.log(dogs);
+
+  //  dog number 1 is an adult and is 5 years old or a puppy
+  dogs.forEach(function (dogs, index) {
+    if (dogs >= 3) {
+      console.log(
+        `dog number ${index + 1} is an adult and is ${dogs} years old `
+      );
+    } else {
+      console.log(`dog ${index + 1} is still a puppy`);
+    }
+  });
+};
+checkDogs([8, 5, 6, 12, 7], [14, 1, 15, 18, 13]);
 */
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurotousd = 1.1;
+
+const EurotoUSD = movements.map(function (items) {
+  return items * eurotousd;
+});
+
+console.log(EurotoUSD);
+console.log(movements);
+
+const deposit = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+console.log(movements);
+console.log(deposit);
+
+const withdrawal = movements.filter(function (mov) {
+  return mov < 0;
+});
+
+console.log(withdrawal);
+*/
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// const balance = movements.reduce(function (acc, curr, i, arr) {
+//   return acc + curr;
+// }, 0);
+
+const balance = movements.reduce((acc, curr) => acc + curr);
+
+console.log(balance);
